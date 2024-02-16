ba90951a017667df29e9a3eea679a5dc4aa8b963
darshan kumawat <114286071+DARSHANKUMAWAT205@users.noreply.github.com>
2024-02-16 07:50:46 +0000

    container config done

---
+++ .devcontainer/01.basic/01.variables.js
@@ -0,0 +1,17 @@
+const accountId = 143322;
+let acccountEmail = "darshan@gmail.com"
+var password = "23343"
+accountCity = "jaipur"
+let accountState;  
+
+//accountId = 2 //not allowed
+
+accountEmail = 'hhh@gmail.com'
+password = '23456'
+accountCity = 'bengalore'
+
+/*  please always use let 
+ because var value can get change for ever */ 
+
+console.log(accountId);
+console.table([accountId,accountEmail, password, accountCity,accountState])
---
+++ .devcontainer/01.basic/02.datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+ // alert(3 + 3); // we are using nodejs, not browser
+
+console.log(3+3);
+//code readability should be high 
+
+console.log("hitesh");
+
+let age = 18;
+let name = "darshan";
+let loggedin = false; 
+
+// number => 2
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof "darshan");
+
+console.log(typeof undefined);  // type is undefined
+console.log(typeof null);   // type is object
+
