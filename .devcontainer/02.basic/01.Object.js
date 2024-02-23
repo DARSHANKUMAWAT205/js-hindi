7428c721a578e0aa312fcd2ac12399563c935c32
DARSHANKUMAWAT205 <dkkumawat2323@gmail.com>
2024-02-23 19:37:24 +0530

    change in code

--- .devcontainer/02.basic/01.Object.js
+++ .devcontainer/02.basic/01.Object.js
@@ -7,16 +7,21 @@ const mySym = Symbol("key1")
 const Ob = {
     name : "darshan",
     "full name" : "darshankumawat",
-    [mySym] : "mykey1",
+    [mySym] : "mykey1",               // [] needed for declaration of Symbol.
     email : "dkkumawat@gamil.com",
     IsLogedIn : false,
     LastlogedIN : ["Monday", "Saturday"]
 }
 
-console.log(Ob.name);
-console.log(Ob["full name"]);
+console.log(Ob.name);          // you can get output of object enteries which is declared using " " colon.
+
+console.log(Ob["full name"]);    // '[]' access any object enteries with square bracket.
+
 console.log(Ob.email);
 console.log(Ob[mySym]);
-
+// Object.freeze(Ob);          // it stop the made change in object 
 Ob.email = "dkumawat@gmail.in"
+
 console.log(Ob.email)
+
+console.log(Ob);
