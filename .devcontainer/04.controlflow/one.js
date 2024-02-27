24889c6f6a1401830d17bca7890bf67c255ba073
DARSHANKUMAWAT205 <dkkumawat2323@gmail.com>
2024-02-27 21:35:51 +0530

    fileadd

---
+++ .devcontainer/04.controlflow/one.js
@@ -0,0 +1,26 @@
+
+// // let num = 15
+// // for (let i = 0; i < num ; i++) {
+// //     const prompt = require("prompt-sync")();
+// //     const num2 = parseInt(prompt("enter your number ="))
+// //     console.log(num2);
+// // }
+// var prompt = require('prompt-sync')();
+// //
+// // get input from the user.
+// //
+// var n = prompt('How many more times? ');
+
+
+// function fizzBuzz(n) {
+//     // Write your code here
+//     if( n % 3==0){
+//         console.log(Fizz);
+//     }
+// }
+
+// function main() {
+//     const n = parseInt(readLine().trim(), 10);
+
+//     fizzBuzz(n);
+// }
