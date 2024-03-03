c3b7a03192b37800bd99fdbd63480225a6d45044
DARSHANKUMAWAT205 <dkkumawat2323@gmail.com>
2024-03-03 21:14:25 +0530

    modified

--- .devcontainer/05.iteration/two.js
+++ .devcontainer/05.iteration/two.js
@@ -1,31 +1,41 @@
 
 // **************** ForIn loop **********
 
-const Myobject = {
-   name :"darshan",
-    lastname:"kumawat",
-    gender:"male",                              // Object
-    occupation:"Student"
+// const Myobject = {
+//    name :"darshan",
+//     lastname:"kumawat",
+//     gender:"male",                              // Object
+//     occupation:"Student"
+// }
+// for (const key in Myobject) {
+//     // console.log(key + ":-" + Myobject[key]);            
+// }
+
+
+// const arr = [ "js" ,"cpp", "py", "java"]            // Array
+
+// for (const key in arr) {
+//     // console.log(key + ":-" + arr[key] )
+// }
+
+
+
+// const map = new Map()
+
+// map.set('In','India')
+// map.set('USA','United State Of America')
+// map.set('Fr','France')
+//                                                     // map are non iterable
+// for (const key in map) {
+//     console.log(key)
+// }
+
+
+var x = 20;
+
+function foo (){
+    console.log(x);
+    var x = 10;
 }
-for (const key in Myobject) {
-    // console.log(key + ":-" + Myobject[key]);            
-}
-
-
-const arr = [ "js" ,"cpp", "py", "java"]            // Array
-
-for (const key in arr) {
-    // console.log(key + ":-" + arr[key] )
-}
-
-
-
-const map = new Map()
-
-map.set('In','India')
-map.set('USA','United State Of America')
-map.set('Fr','France')
-                                                    // map are non iterable
-for (const key in map) {
-    console.log(key)
-}
+foo ()
+console.log(x)
